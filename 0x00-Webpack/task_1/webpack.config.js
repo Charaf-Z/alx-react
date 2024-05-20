adcc05368d61d0ad591f5fd98ee5552149682275
Charaf-Z <charafomarzitouni@gmail.com>
2024-05-20 17:21:51 +0100

    check for config

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -3,10 +3,10 @@ const path = require('path');
 module.exports = {
   mode: 'production',
   entry: {
-    main: path.resolve(__dirname, 'js/dashboard_main.js'),
+    bundle: path.resolve(__dirname, 'js/dashboard_main.js'),
   },
   output: {
     path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js',
+    filename: '[name].js',
   },
 };
