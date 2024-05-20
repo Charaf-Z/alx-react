62596cd3bcd07600a2d6be684a89adda11884429
Charaf-Z <charafomarzitouni@gmail.com>
2024-05-20 17:47:50 +0100

    update code

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,11 +1,5 @@
-import $ from 'jquery';
-import _ from 'lodash';
-
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append('<button>Click here to get started</button>');
-$('body').append("<p id='count'></p>");
-$('body').append('<p>Copyright - Holberton School</p>');
+const $ = require('jquery');
+const _ = require('lodash');
 
 let count = 0;
 
@@ -13,4 +7,13 @@ function updateCounter() {
   $('#count').html(`${++count} clicks on the button`);
 }
 
-$('button').on('click', _.debounce(updateCounter, 500));
+const btn = $('<button>Click here to get started</button>').on(
+  'click',
+  _.debounce(updateCounter, 500)
+);
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append(btn);
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
