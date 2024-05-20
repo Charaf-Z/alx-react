825acf4aa98be1f60ba02f3f33b79f6bc71e161c
Charaf-Z <charafomarzitouni@gmail.com>
2024-05-20 17:59:55 +0100

    change the main file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -3,18 +3,15 @@ const _ = require('lodash');
 
 let count = 0;
 
+$('<p>Holberton Dashboard</p>').appendTo('body');
+$('<p>Dashboard data for the students</p>').appendTo('body');
+$('<button>Click here to get started</button>').appendTo('body');
+$("<p id='count'></p>").appendTo('body');
+$('<p>Copyright - Holberton School</p>').appendTo('body');
+
 function updateCounter() {
   count++;
   $('#count').html(`${count} clicks on the button`);
 }
 
-const btn = $('<button>Click here to get started</button>').on(
-  'click',
-  _.debounce(updateCounter, 500)
-);
-
-$('body').append('<p>Holberton Dashboard</p>');
-$('body').append('<p>Dashboard data for the students</p>');
-$('body').append(btn);
-$('body').append("<p id='count'></p>");
-$('body').append('<p>Copyright - Holberton School</p>');
+$('button').on('click', _.debounce(updateCounter, 500));
