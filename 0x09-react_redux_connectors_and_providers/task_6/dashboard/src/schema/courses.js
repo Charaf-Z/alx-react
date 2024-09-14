4a3b673cdcbf6f94850e1976365cf4080374c6bb
Charaf-Z <charafomarzitouni@gmail.com>
2024-09-14 01:54:47 +0100

    back one month

--- 0x09-react_redux_connectors_and_providers/task_6/dashboard/src/schema/courses.js
+++
@@ -1,4 +0,0 @@
-const { schema, normalize } = require('normalizr');
-
-const courseSchema = new schema.Entity('courses');
-export const coursesNormalizer = (data) => normalize(data, [courseSchema]);
